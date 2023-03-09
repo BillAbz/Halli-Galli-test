01dff92276a3a826cadd58582882eaf8371708b3
BillAbz <billabz23@gmail.com>
2023-03-09 14:49:02 +0100

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module be.kdg.halligallitest {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens be.kdg.halligallitest to javafx.fxml;
+    exports be.kdg.halligallitest;
+}
